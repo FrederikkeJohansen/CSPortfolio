14999b7053c2ca697206fdc8bd81aeabed4e0ad5
Frederikke Johansen <frederikke.ek.johansen@gmail.com>
2026-02-20 15:05:07 +0100

    Added shadcn/UI button

--- csportfolio/components/CourseFilter.tsx
+++ csportfolio/components/CourseFilter.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@/components/ui/button"
 export default function CourseFilter() {
     return (
         <div className="mb-8">
             <div className="flex gap-4 justify-center">
-                <button className="px-4 py-2 bg-blue-500 text-white rounded">Physcom</button>
-                <button className="px-4 py-2 bg-blue-500 text-white rounded">Noget</button>
-                <button className="px-4 py-2 bg-blue-500 text-white rounded">HCI</button>
+                <Button variant="default" className="bg-blue-500 text-white rounded">Physcom</Button>
+                <Button variant="default" className="bg-blue-500 text-white rounded">Noget</Button>
+                <Button variant="default" className="bg-blue-500 text-white rounded">HCI</Button>
             </div>
         </div>
     )

385a970fb64209b5a1a0843f132c9603b5aea8a4
Frederikke Johansen <frederikke.ek.johansen@gmail.com>
2026-02-24 11:32:45 +0100

    Design for darkmode

--- csportfolio/components/ProjectCard.tsx
+++ csportfolio/components/ProjectCard.tsx
@@ -12,7 +12,7 @@ export default function ProjectCard({ project }: ProjectCardProps) {
     return (
         <div className="h-[300px] md:h-[360px] xl:h-[400px] bg-indigo-100 dark:bg-zinc-900 rounded-xl overflow-hidden shadow flex flex-col cursor-pointer">
             {/* Image */}
-            <div className="relative w-full h-36 md:h-44 xl:h-48 bg-zinc-200 dark:bg-zinc-700">
+            <div className="relative w-full h-36 md:h-44 xl:h-48">
                 {imageSrc && (
                     <Image
                         src={imageSrc}
@@ -26,7 +26,7 @@ export default function ProjectCard({ project }: ProjectCardProps) {
 
             {/* Content */}
             <div className="p-4 flex flex-col gap-1 flex-1 overflow-hidden">
-                <p className="text-xs font-semibold tracking-wide uppercase text-indigo-400 dark:text-indigo-400">
+                <p className="text-xs font-semibold tracking-wide uppercase text-indigo-400 dark:text-indigo-300">
                     {courseName && <span>{courseName}</span>}
                     {courseName && project.year && <span> • </span>}
                     {project.year && <span>{project.year}</span>}
@@ -34,12 +34,12 @@ export default function ProjectCard({ project }: ProjectCardProps) {
                 <h2 className="font-bold text-xl leading-snug line-clamp-1 text-gray-900 dark:text-white">{project.title}</h2>
 
                 {project.description && (
-                    <p className="text-sm text-gray-700 dark:text-zinc-400 line-clamp-2 md:line-clamp-3 leading-relaxed">
+                    <p className="text-sm text-gray-700 dark:text-zinc-200 line-clamp-2 md:line-clamp-3 leading-relaxed">
                         {project.description}
                     </p>
                 )}
                 {project.student_creators && (
-                    <p className="text-xs text-gray-700 dark:text-zinc-500 mt-auto pt-2 truncate">
+                    <p className="text-xs font-semibold text-gray-700 dark:text-zinc-200 mt-auto pt-2 truncate">
                         Created by: {project.student_creators}
                     </p>
                 )}

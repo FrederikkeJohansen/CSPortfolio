7c8108ac17ee2b36f1a82fd11bd23876d762590a
Frederikke Johansen <frederikke.ek.johansen@gmail.com>
2026-02-25 13:34:11 +0100

    Added "upload project button"

--- csportfolio/components/Navbar.tsx
+++ csportfolio/components/Navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
                 <h1 className="text-4xl font-bold text-black dark:text-white">Something really cool, and maybe a logo?
                 </h1>
                 <div className="flex gap-2">
-                    <Button variant="outline">Upload</Button>
+                    <Button variant="upload" className="text-xs dark:active:text-indigo-300 font-semibold tracking-wide uppercase text-indigo-500 dark:text-indigo-300">Upload projects</Button>
                     <Button variant="outline">Search</Button>
                 </div>
             </div>

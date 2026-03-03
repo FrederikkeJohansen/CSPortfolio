586deab7fa4fe2c42901f1899d28194dc3b65b29
Frederikke Johansen <frederikke.ek.johansen@gmail.com>
2026-03-03 21:26:15 +0100

    dark

--- csportfolio/app/projects/[id]/page.tsx
+++ csportfolio/app/projects/[id]/page.tsx
@@ -40,29 +40,29 @@ export default async function ProjectPage({ params }: Props) {
                     <h1 className="text-2xl md:text-4xl font-bold text-zinc-800 dark:text-zinc-200 mb-4">{project?.title}</h1>
 
                     <p className="text-sm xl:text-xl font-medium text-black dark:text-zinc-300 mb-1">{project?.description}</p>
-                    <p className="text-sm text-sm xl:text-xl text-black dark:text-white mb-1"><span className="uppercase tracking-wide font-bold text-black dark:text-indigo-300">Key Words: </span>something</p>
+                    <p className="text-sm text-sm xl:text-xl text-black dark:text-white mb-1"><span className="uppercase tracking-wide font-bold text-black dark:text-white">Key Words: </span>Something</p>
 
                     <p className="text-sm font-bold text-zinc-500 dark:text-zinc-300 mt-4">Created by: {project?.student_creators}</p>
 
                     <div className="flex flex-row gap-4 text-sm md:text-base font-medium mt-4">
                         {project?.video_url ? (
                             <a href={project.video_url} target="_blank" rel="noopener noreferrer"
-                                className="text-indigo-500 dark:text-indigo-300 hover:underline font-bold cursor-pointer">
+                                className="text-indigo-500 dark:text-indigo-200 hover:underline font-bold cursor-pointer">
                                 See video
                             </a>
                         ) : (
-                            <span title="No video added" className="text-md font-bold text-zinc-400 dark:text-zinc-600 cursor-default">
+                            <span title="No video added" className="text-md font-bold text-zinc-400 dark:text-zinc-500 cursor-default">
                                 See video
                             </span>
                         )}
 
                         {project?.poster_url ? (
                             <a href={project.poster_url} target="_blank" rel="noopener noreferrer"
-                                className="text-indigo-500 dark:text-indigo-300 hover:underline font-bold cursor-pointer">
+                                className="text-indigo-500 dark:text-indigo-200 hover:underline font-bold cursor-pointer">
                                 See poster
                             </a>
                         ) : (
-                            <span title="No poster added" className="text-md font-bold text-zinc-400 dark:text-zinc-600 cursor-default">
+                            <span title="No poster added" className="text-md font-bold text-zinc-400 dark:text-zinc-500 cursor-default">
                                 See poster
                             </span>
                         )}

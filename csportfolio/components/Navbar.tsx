0be5a0dc82176b2642bce3033140d83d8780ee54
Frederikke Johansen <frederikke.ek.johansen@gmail.com>
2026-03-04 12:51:25 +0100

    navbar styling fixed

--- csportfolio/components/Navbar.tsx
+++ csportfolio/components/Navbar.tsx
@@ -10,21 +10,19 @@ export default function Navbar() {
     const [uploadOpen, setUploadOpen] = useState(false)
     return (
         <>
-            <nav className="bg-white dark:bg-zinc-900 shadow">
-                <div className="p-8 flex items-center justify-between">
-                    <h1 className="text-4xl font-bold text-black dark:text-white">Something really cool, and maybe a logo?
-                    </h1>
+
+            <nav className="w-full sticky top-0 z-50 bg-zinc-50/80 dark:bg-black/80 backdrop-blur-sm">
+                <div className="px-8 py-4 flex items-center justify-end md:justify-between">
+                    <p className="hidden md:block text-sm font-semibold tracking-widest uppercase text-zinc-400 dark:text-zinc-500 select-none">
+                        The student project portfolio - upload & explore
+                    </p>
                     <div className="flex gap-2 items-center">
                         <Button variant="upload" onClick={() => setUploadOpen(true)} className="text-xs dark:active:text-indigo-300 font-semibold tracking-wide uppercase text-indigo-500 dark:text-indigo-300">Upload project</Button>
                         <SearchBar />
                     </div>
                 </div>
-
             </nav>
             <UploadModal open={uploadOpen} onClose={() => setUploadOpen(false)} />
-
         </>
     );
-
-
 }

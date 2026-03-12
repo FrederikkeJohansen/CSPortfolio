c5ca7c42f54f8c6a154125c0cdf9dcc6514e6d62
Frederikke Johansen <frederikke.ek.johansen@gmail.com>
2026-03-12 07:59:46 +0100

    nevet gets cached

--- csportfolio/app/admin/page.tsx
+++ csportfolio/app/admin/page.tsx
@@ -2,11 +2,12 @@ import { getProjectInformationForAdmin, getAllCourses, getAllPassphrases } from 
 import { AdminDashboard } from '@/components/admin/AdminDashboard'
 
 export default async function AdminPage() {
-  const [projects, courses, passphrases] = await Promise.all([
-    getProjectInformationForAdmin(),
-    getAllCourses(),
-    getAllPassphrases(),
-  ])
+  export default async function AdminPage() {
+    const [projects, courses, passphrases] = await Promise.all([
+      getProjectInformationForAdmin(),
+      getAllCourses(),
+      getAllPassphrases(),
+    ])
 
-  return <AdminDashboard projects={projects} courses={courses} passphrases={passphrases} />
-}
+    return <AdminDashboard projects={projects} courses={courses} passphrases={passphrases} />
+  }
